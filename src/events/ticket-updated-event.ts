f0634114c1919ca6bdaaa3ee85c4671126508727
Parth Ahuja <aryaanahuja14@gmail.com>
2023-01-24 15:10:37 +0530

    Updates

--- src/events/ticket-updated-event.ts
+++ src/events/ticket-updated-event.ts
@@ -1,6 +1,6 @@
 import { Subjects } from "./subjects";
 
-export interface TicketUpdated {
+export interface TicketUpdatedEvent {
     subject: Subjects.TicketUpdated,
     data: {
         id: string;
