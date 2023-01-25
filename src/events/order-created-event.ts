43a8b456d3099dc83c34a80ce14c0aee2c90db35
Parth Ahuja <aryaanahuja14@gmail.com>
2023-01-25 16:31:35 +0530

    Updates

---
+++ src/events/order-created-event.ts
@@ -0,0 +1,16 @@
+import { Subjects } from "./subjects";
+import { OrderStatus } from "./types/order-status";
+
+export interface OrderCreatedEvent {
+    subject: Subjects.OrderCreated;
+    data: {
+        id: string;
+        status: OrderStatus;
+        userId: string;
+        expiresAt: string; // events are only emitted as string not date
+        ticket: {
+            id: string;
+            price: number;
+        };
+    };
+};
