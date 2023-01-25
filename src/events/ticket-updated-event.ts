14527cd03c5998cf18471fb4b609df3245760aa4
Parth Ahuja <aryaanahuja14@gmail.com>
2023-01-25 18:57:57 +0530

    Updates

--- src/events/ticket-updated-event.ts
+++ src/events/ticket-updated-event.ts
@@ -4,8 +4,9 @@ export interface TicketUpdatedEvent {
     subject: Subjects.TicketUpdated,
     data: {
         id: string;
+        version: number;
         title: string;
         price: number;
         userId: string;
-    }
-}
+    },
+};
