8aa050f37d8c8f6c7c3062ff47192e32dd67b4df
Parth Ahuja <aryaanahuja14@gmail.com>
2023-01-28 01:41:54 +0530

    Updates

--- src/index.ts
+++ src/index.ts
@@ -17,5 +17,6 @@ export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
 export * from './events/order-cancelled-event';
 export * from './events/order-created-event';
+export * from './events/expiration-completed-event'
 
 export * from './events/types/order-status';
